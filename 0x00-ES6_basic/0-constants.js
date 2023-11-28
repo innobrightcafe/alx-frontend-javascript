40f22f067ba1c5b01e7a3184e5719c6f3a6f7caf
inosoft <innobrightcafe@gmail.com>
2023-11-28 01:27:42 -0500

    update on const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,11 +2,11 @@ export function taskFirst() {
     var task = 'I prefer const when I can.';
     return task;
   }
-  
+
   export function getLast() {
     return ' is okay';
   }
-  
+
   export function taskNext() {
     var combination = 'But sometimes let';
     combination += getLast();
