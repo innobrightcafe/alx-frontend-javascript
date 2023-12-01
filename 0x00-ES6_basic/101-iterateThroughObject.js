f36564e64c41296346986a0fc02b6fbc0e60adf0
inosoft <innobrightcafe@gmail.com>
2023-12-01 09:32:49 -0500

    new update

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 const iterateThroughObject = (reportWithIterator) => {
   let separate = '';
   for (const e of reportWithIterator) {
-    separate += `${e} ' | '`;
+    separate += `${e} | `;
   }
   separate = separate.slice(0, -3);
   return separate;
