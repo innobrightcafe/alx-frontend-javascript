968262ea544b62d51de552d17e15ee5f061aedf2
inosoft <innobrightcafe@gmail.com>
2023-12-03 01:29:47 -0500

    the fake promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,13 @@
-const getResponseFromAPI = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
+const getResponseFromAPI = () => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const success = true;
 
-      if (success) {
-        resolve("API response data");
-      } else {
-        reject("API request failed");
-      }
-    }, 1000);
-  });
-};
+    if (success) {
+      resolve('API response data');
+    } else {
+      reject(new Error('Something went wrong - error '));
+    }
+  }, 1000);
+});
 
 export default getResponseFromAPI;
