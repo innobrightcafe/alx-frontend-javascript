90a57e785426716be76bec2d7ac56aa43bc29a3a
inosoft <innobrightcafe@gmail.com>
2023-12-05 07:27:33 -0500

    set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(inputSet, startString) {
   const cleanedValues = Array.from(inputSet)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length));
+    .filter(value => value.startsWith(startString))
+    .map(value => value.substring(startString.length))
+    .join('-');
 
-  return cleanedValues.join("-");
+  return cleanedValues;
 }
